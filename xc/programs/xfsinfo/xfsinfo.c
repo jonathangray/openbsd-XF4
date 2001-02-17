a2d5a635f4fe2643f9e5697af5b0ba6368bf2e07
matthieu <matthieu@openbsd.org>
2001-02-17 17:45:34 +0000

    rename fsinfo -> xfsinfo to avoid conflict with am-utils

--- xc/programs/xfsinfo/xfsinfo.c
+++ xc/programs/xfsinfo/xfsinfo.c
@@ -1,5 +1,5 @@
 /*
- * fsinfo -- report info about a font server
+ * xfsinfo -- report info about a font server
  */
 
 /* $TOG: fsinfo.c /main/7 1998/02/09 13:43:08 kaleb $ */
@@ -26,7 +26,7 @@ other dealings in this Software without prior written authorization
 from The Open Group.
 
 */
-/* $XFree86: xc/programs/fsinfo/fsinfo.c,v 3.3 1999/02/19 21:27:07 hohndel Exp $ */
+/* $XFree86: xc/programs/xfsinfo/xfsinfo.c,v 1.1 2001/01/06 21:58:28 herrb Exp $ */
 
 /*
  * Copyright 1990 Network Computing Devices;
