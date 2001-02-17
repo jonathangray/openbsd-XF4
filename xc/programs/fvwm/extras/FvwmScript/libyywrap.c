33417e863887b316353c19acb24f46719f1dba94
matthieu <matthieu@openbsd.org>
2001-02-17 22:21:23 +0000

    import fvwm 2.0.46

---
+++ xc/programs/fvwm/extras/FvwmScript/libyywrap.c
@@ -0,0 +1,8 @@
+/* libyywrap - flex run-time support library "yywrap" function */
+
+/* $Header: /tmp/OpenBSD-XF4-repo/xc/programs/fvwm/extras/FvwmScript/libyywrap.c,v 1.1.1.1 2001/02/17 22:22:33 matthieu Exp $ */
+
+int yywrap()
+	{
+	return 1;
+	}
