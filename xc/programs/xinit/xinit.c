d708ac48675799f3a51c1a1a880427d0e55aed5e
matthieu <matthieu@openbsd.org>
2001-10-07 16:36:27 +0000

    add some knowledge of Xwrapper logic for 3.3.6 servers to xinit, so that these servers can be started by Xwrapper without the xserverrc kludge that was needed in 2.9. If the target of the X link contains "XF86_", then the server will be started by Xwrapper. Otherwise we assume that it can be started directly.
    This can fail in obscure ways if the user has a strange non-standard
    directory layout, but I hope it will bring benefits to the waste majority
    and reduce the number of "normal users cannot start X" complaints.

--- xc/programs/xinit/xinit.c
+++ xc/programs/xinit/xinit.c
@@ -28,6 +28,8 @@ in this Software without prior written authorization from The Open Group.
 #include <stdio.h>
 #include <ctype.h>
 
+#include <sys/param.h>
+
 #ifdef X_POSIX_C_SOURCE
 #define _POSIX_C_SOURCE X_POSIX_C_SOURCE
 #include <signal.h>
@@ -148,6 +150,9 @@ char xserverrcbuf[256];
 #define	OK_EXIT		0
 #define	ERR_EXIT	1
 
+char *default_wrapper = BINDIR "/Xwrapper";
+char real_server[MAXPATHLEN];
+char test_path[MAXPATHLEN];
 char *default_server = "X";
 char *default_display = ":0";		/* choose most efficient */
 #ifndef __EMX__
@@ -248,7 +253,8 @@ main(int argc, char *argv[], char *envp[])
 	register char **sptr = server;
 	register char **cptr = client;
 	register char **ptr;
-	int pid;
+	char *path, *p;
+	int pid, n;
 	int client_given = 0, server_given = 0;
 	int client_args_given = 0, server_args_given = 0;
 	int start_of_client_args, start_of_server_args;
@@ -317,7 +323,36 @@ main(int argc, char *argv[], char *envp[])
 	if (argc == 0 ||
 #ifndef __EMX__
 	    (**argv != '/' && **argv != '.')) {
-		*sptr++ = default_server;
+	    /* hack for the Xfree86 3.3.6 servers: if the X link 
+	       points to XF86_*, start Xwapper instead if X */
+		path = strdup(getenv("PATH"));
+		if (path == NULL) {
+		    Error("Can't find PATH\n");
+		    exit(1);
+		}
+		/* Walk through the PATH */
+		for (p = strtok(path, ":"); p != NULL; p = strtok(NULL, ":")) {
+		    strlcpy(test_path, p, sizeof(test_path));
+		    strlcat(test_path, "/", sizeof(test_path));
+		    strlcat(test_path, default_server, sizeof(test_path));
+		    if ((n = readlink(test_path, real_server, 
+				 sizeof(real_server) - 1)) > 0) {
+			real_server[n] = '\0';
+			/* if the target path contains XF86_, we 
+			   need Xwrapper */
+			if (strstr(real_server, "XF86_") != NULL) {
+			    *sptr++ = default_wrapper;
+			} else {
+			    *sptr++ = default_server;
+			}
+			break;
+		    }
+		}
+		if (p == NULL) {
+		    Error("Can't find X link\n");
+		    exit(1);
+		}
+		free(path);
 #else
 	    (**argv != '/' && **argv != '\\' && **argv != '.' &&
 	     !(isalpha(**argv) && (*argv)[1]==':'))) {
