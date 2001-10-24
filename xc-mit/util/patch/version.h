934902d36675d4d43cf4b33426b8e9e7d201e246
todd <todd@openbsd.org>
2001-10-24 13:12:59 +0000

    This started out as X11R5, with mods from Jason Downs, and code from 4.4BSD-lite.  This 'xc-mit' tree's purpose is to build XhpBSD for hp300, anything it requires, and NOTHING else.

---
+++ xc-mit/util/patch/version.h
@@ -0,0 +1,14 @@
+/* $Header: /tmp/OpenBSD-XF4-repo/xc-mit/util/patch/Attic/version.h,v 1.1.1.1 2001/10/24 13:13:01 todd Exp $
+ *
+ * $Log: version.h,v $
+ * Revision 1.1.1.1  2001/10/24 13:13:01  todd
+ * This started out as X11R5, with mods from Jason Downs, and code from
+ * 4.4BSD-lite.  This 'xc-mit' tree's purpose is to build XhpBSD for hp300,
+ * anything it requires, and NOTHING else.
+ *
+ * Revision 2.0  86/09/17  15:40:14  lwall
+ * Baseline for netwide release.
+ * 
+ */
+
+void version();
