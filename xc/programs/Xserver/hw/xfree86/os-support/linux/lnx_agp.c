d1b4fb5cf847f9dd3dd2a121bb5dac435d8a5f64
matthieu <matthieu@openbsd.org>
2002-04-22 21:57:42 +0000

    Add support for HasAgpGart for OpenBSD (millert@)

--- xc/programs/Xserver/hw/xfree86/os-support/linux/lnx_agp.c
+++ xc/programs/Xserver/hw/xfree86/os-support/linux/lnx_agp.c
@@ -18,7 +18,7 @@
 #if defined(linux)
 #include <asm/ioctl.h>
 #include <linux/agpgart.h>
-#elif defined(__FreeBSD__) || defined(__NetBSD__)
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
 #include <sys/ioctl.h>
 #include <sys/agpio.h>
 #endif
