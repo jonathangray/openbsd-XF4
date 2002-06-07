e40cdb63cefc8bf210bb04188d4f0ec227cddffe
matthieu <matthieu@openbsd.org>
2002-06-07 18:48:03 +0000

    Copyright + rcs id

--- xc/programs/Xserver/hw/kdrive/wsfb/wsmouse.c
+++ xc/programs/Xserver/hw/kdrive/wsfb/wsmouse.c
@@ -1,12 +1,33 @@
-/**
- ** Copyright (c) 2002 Matthieu Herrb
- **
- ** $Source: /tmp/OpenBSD-XF4-repo/xc/programs/Xserver/hw/kdrive/wsfb/Attic/wsmouse.c,v $
- ** $Revision: 1.1 $
- ** $Date: 2002/06/07 07:02:30 $
- ** 
- **/
-#ident "$Id: wsmouse.c,v 1.1 2002/06/07 07:02:30 matthieu Exp $"
+/* $OpenBSD: wsmouse.c,v 1.2 2002/06/07 18:48:49 matthieu Exp $ */
+/*
+ * Copyright (c) 2001 Matthieu Herrb
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 #define NEED_EVENTS
 #include "X.h"
