a19b11d3086cbe146776c544e74398e7c01ae203
matthieu <matthieu@openbsd.org>
2003-02-25 21:53:09 +0000

    - always return an empty window or icon title. (CAN-2003-0063) - update the OpenBSD version in xterm -v

--- xc/programs/xterm/version.h
+++ xc/programs/xterm/version.h
@@ -1,10 +1,11 @@
 /* $XFree86: xc/programs/xterm/version.h,v 3.77 2002/01/08 01:40:29 dawes Exp $ */
-
+/* $OpenBSD: version.h,v 1.3 2003/02/25 21:53:09 matthieu Exp $ */
 /*
  * These definitions are used to build the string that's printed in response to
  * "xterm -version", or embedded in "xterm -help".  It is the version of
  * XFree86 to which this version of xterm has been built.  The number in
  * parentheses is my patch number (T.Dickey).
  */
+/* Add a specific version for OpenBSD, since there are local changes */
 #define XTERM_PATCH   165
-#define XFREE86_VERSION "XFree86 4.2.0/OpenBSD 3.1"
+#define XFREE86_VERSION "XFree86 4.2.1/OpenBSD 3.3"
