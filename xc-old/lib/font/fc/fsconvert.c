a4c7ae352fba3cc6a8477e5105fec5c64c4ba7d3
matthieu <matthieu@openbsd.org>
2003-08-30 07:45:19 +0000

    Potential integer overflows in the 3.3.6 X server.

--- xc-old/lib/font/fc/fsconvert.c
+++ xc-old/lib/font/fc/fsconvert.c
@@ -105,6 +105,10 @@ _fs_convert_props(pi, po, pd, pfi)
 
     nprops = pfi->nprops = pi->num_offsets;
 
+    if (nprops < 0 
+       || nprops > SIZE_T_MAX/(sizeof(FontPropRec) + sizeof(char))) 
+       return -1;
+
     dprop = (FontPropPtr) xalloc(sizeof(FontPropRec) * nprops);
     is_str = (char *) xalloc(sizeof(char) * nprops);
     if (!dprop || !is_str) {
