939e6a306923e140d9a9661d5d03be25c770d7ad
matthieu <matthieu@openbsd.org>
2003-12-14 17:51:29 +0000

    remove empty line(s) at end of file

--- xc/programs/Xserver/hw/xfree86/dummylib/dispatchException.c
+++ xc/programs/Xserver/hw/xfree86/dummylib/dispatchException.c
@@ -1,9 +1,8 @@
 /**
  ** Public domain
  **/
-/* $OpenBSD: dispatchException.c,v 1.1 2002/09/01 16:29:36 matthieu Exp $ */
+/* $OpenBSD: dispatchException.c,v 1.2 2003/12/14 17:51:29 matthieu Exp $ */
 
 #include "opaque.h"
 
 char dispatchException;
-
--- xc/programs/Xserver/hw/xfree86/dummylib/serverGeneration.c
+++ xc/programs/Xserver/hw/xfree86/dummylib/serverGeneration.c
@@ -1,9 +1,8 @@
 /**
  ** Public domain
  **/
-/* $OpenBSD: serverGeneration.c,v 1.1 2002/09/01 16:29:36 matthieu Exp $ */
+/* $OpenBSD: serverGeneration.c,v 1.2 2003/12/14 17:51:30 matthieu Exp $ */
 
 #include "misc.h"
 
 unsigned long serverGeneration;
-
