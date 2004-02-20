5f3c17236a42a7fcb3a49982160f969a813dbdf0
matthieu <matthieu@openbsd.org>
2004-02-20 22:33:09 +0000

    Merge back local change that got lost in the automatic merge. Fixes 'black text mode on VT switch' problem noticed by several people with XFree86 4.3.99.902.

--- xc/programs/Xserver/hw/xfree86/drivers/ati/radeon_driver.c
+++ xc/programs/Xserver/hw/xfree86/drivers/ati/radeon_driver.c
@@ -5497,7 +5497,7 @@ static void RADEONSave(ScrnInfoPtr pScrn)
 	 */
 	vgaHWSave(pScrn, &hwp->SavedReg, VGA_SR_MODE); /* Save mode only */
 #else
-	vgaHWSave(pScrn, &hwp->SavedReg, VGA_SR_MODE | VGA_SR_FONTS); /* Save mode
+	vgaHWSave(pScrn, &hwp->SavedReg, VGA_SR_ALL); /* Save mode
 						       * & fonts & cmap
 						       */
 #endif
