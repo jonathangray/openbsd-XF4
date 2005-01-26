d05a4dc846fa78c5b389e7c0d7f60029f35051c1
miod <miod@openbsd.org>
2005-01-26 18:45:22 +0000

    Wave goodbye to the old hp300 X11R5 server.

--- xc-mit/lib/Xdmcp/Wrap.h
+++
@@ -1,9 +0,0 @@
-/*
- * header file for compatibility with something useful
- */
-
-typedef unsigned char auth_cblock[8];	/* block size */
-
-typedef struct auth_ks_struct { auth_cblock _; } auth_wrapper_schedule[16];
-
-extern void _XdmcpWrapperToOddParity();
