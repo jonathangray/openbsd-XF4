005570fb3e462c2a91c6500d5abd0f78f4848619
matthieu <matthieu@openbsd.org>
2006-01-21 22:29:53 +0000

    don't use exec since do_exit needs to be run on exit. Noticed by david@.

--- xc/programs/xdm/config/Xsession.cpp
+++ xc/programs/xdm/config/Xsession.cpp
@@ -1,7 +1,7 @@
 XCOMM!SHELL_CMD
 XCOMM
 XCOMM $Xorg: Xsession,v 1.4 2000/08/17 19:54:17 cpqbld Exp $
-XCOMM $OpenBSD: Xsession.cpp,v 1.1 2006/01/21 22:12:12 matthieu Exp $
+XCOMM $OpenBSD: Xsession.cpp,v 1.2 2006/01/21 22:29:53 matthieu Exp $
 XCOMM $XFree86: xc/programs/xdm/config/Xsession,v 1.2 1998/01/11 03:48:32 dawes Exp $
 
 XCOMM redirect errors to a file in user's home directory if we can
@@ -42,7 +42,7 @@ case $# in
 1)
 	case $1 in
 	failsafe)
-		exec BINDIR/xterm -geometry 80x24-0-0
+		BINDIR/xterm -geometry 80x24-0-0
 		do_exit
 		;;
 	esac
@@ -55,9 +55,9 @@ resources=$HOME/.Xresources
 
 if [ -s "$startup" ]; then
 	if [ -x "$startup" ]; then
-		exec "$startup"
+		"$startup"
 	else
-		exec /bin/sh "$startup"
+		/bin/sh "$startup"
 	fi
 else
 	if [ -f "$resources" ]; then
@@ -82,6 +82,6 @@ else
         }
 #endif
 	BINDIR/xterm &
-	exec BINDIR/fvwm
+	BINDIR/fvwm
 fi
 do_exit
