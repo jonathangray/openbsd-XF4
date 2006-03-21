4a3a034574075161127bdb78c056690560689422
matthieu <matthieu@openbsd.org>
2006-03-21 18:53:58 +0000

    Provide a dummy priv_open_device() function for utilities like pcitweak.

--- xc/programs/Xserver/hw/xfree86/dummylib/privsep.c
+++ xc/programs/Xserver/hw/xfree86/dummylib/privsep.c
@@ -1,11 +1,18 @@
 /**
  ** Public domain
  **/
-/* $OpenBSD: privsep.c,v 1.2 2003/04/01 22:36:49 matthieu Exp $ */
+/* $OpenBSD: privsep.c,v 1.3 2006/03/21 18:53:58 matthieu Exp $ */
 #include <sys/types.h>
+#include <fcntl.h>
 
 int 
 priv_init(uid_t uid, gid_t gid)
 {
 	return 0;
+}
+
+int
+priv_open_device(char *path)
+{
+	return open(path, O_RDWR);
 }
